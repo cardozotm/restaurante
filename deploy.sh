4e6402203fd2c571785d35bff4f9a68b993e9090
tcardozo <tcardozo@bluchain.tech>
2019-04-15 19:28:22 -0300

    Fix auto deploy on ./deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ VALID="$(npm test | grep -o 'failing')"
 
 if [[ $VALID != "failing" ]]
    then
-xterm -e "cd $cwd/restauranteApi && sudo ./container-up.sh; bash" &
-xterm -e "cd $cwd/restauranteApp && sudo ./container-up.sh; bash" &
+cd $cwd/restauranteApi && sudo ./container-up.sh; bash &
+cd $cwd/restauranteApp && sudo ./container-up.sh; bash &
 
 fi
